e013ce94a1eb8b6d4979887eafdff338c55ded18
linlanggirl <857123441@qq.com>
2017-09-01 20:14:41 +0800

    注释

--- chapter3/canny.py
+++ chapter3/canny.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+# 边缘检测
 img = cv2.imread("../images/statue_small.jpg", 0)
 cv2.imwrite("canny.jpg", cv2.Canny(img, 200, 300))
 cv2.imshow("canny", cv2.imread("canny.jpg"))
--- chapter3/contours.py
+++ chapter3/contours.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+# 轮廓检测
 img = np.zeros((200, 200), dtype=np.uint8)
 img[50:150, 50:150] = 255
 
